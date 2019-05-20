4cda66858d4eb294c47171d6e69c5af0cb6fc681
trinhvanvinh <trinhvanvinh018@gmail.com>
2019-05-20 23:25:36 +0700

    das

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import Product from './Product';
+import Title from './Title';
+import {storeProducts} from '../data';
 
 export default class ProductList extends Component {
   state={
-    products:[]
+    products:storeProducts
   }
   render() {
+    console.log(this.state.products);
     return (
      <React.Fragment>
+      <div className="py-5" >
+        <div className="container" >
+          <Title name="our" title="products" />
+          <div className="row" />
 
+        </div>
+      </div>
      </React.Fragment>
     )
   }
