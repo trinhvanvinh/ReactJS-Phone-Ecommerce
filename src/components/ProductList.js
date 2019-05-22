f8aeceaa842290da6c779ab95a9ac8276ec76585
mac <>
2019-05-22 17:28:28 +0700

    1.47

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Product from './Product';
 import Title from './Title';
 import {storeProducts} from '../data';
+import {ProductConsumer} from './context';
 
 export default class ProductList extends Component {
   state={
@@ -14,7 +15,13 @@ export default class ProductList extends Component {
       <div className="py-5" >
         <div className="container" >
           <Title name="our" title="products" />
-          <div className="row" />
+          <div className="row" >
+            <ProductConsumer>
+              {(hello)=>{
+                return <h1>{hello}</h1>
+              }}
+            </ProductConsumer>
+          </div>
 
         </div>
       </div>
@@ -22,4 +29,3 @@ export default class ProductList extends Component {
     )
   }
 }
-// 1.21
--- src/components/context.js
+++ src/components/context.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import {storeProducts, datailProduct} from '../data';
 const ProductContext= React.createContext();
 // provider
 
 //comsumer
 
  class ProductProvider extends Component {
+   state={
+     products: storeProducts,
+     detailProduct: datailProduct
+   }
+   handleDetail = ()=>{
+     console.log("helo from detail")
+   }
+   addToCart=()=>{
+    console.log("helo from cart")
+   }
   render() {
     return (
-     <ProductContext.Provider value="hello from context" >
+     <ProductContext.Provider value={{
+       ...this.state, handleDetail: this.handleDetail, addToCart: this.addToCart
+     }} >
          {this.props.children}
      </ProductContext.Provider>
     )
@@ -16,5 +29,3 @@ const ProductContext= React.createContext();
 
 const ProductConsumer= ProductContext.Consumer;
 export {ProductProvider, ProductConsumer};
-
-//1.35
