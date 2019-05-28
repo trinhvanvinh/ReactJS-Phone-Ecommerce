cd8ab0cc8aba8f2ae4bf4ff33500256bca97b07d
trinhvanvinh <trinhvanvinh018@gmail.com>
2019-05-28 23:05:24 +0700

    update coide

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Title from './Title';
-
+import CartColumns from './CartColumns'
 export default class Cart extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <section>
+        <Title name="your" title="cart" />
+        <CartColumns />
+      </section>
     )
   }
 }
