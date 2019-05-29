8bbfd24700a2878055d62536f4c1d5307675eb12
mac <>
2019-05-29 11:17:08 +0700

    update code 4.50

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,10 +1,29 @@
 import React from 'react'
 
-export default function CartItem() {
+export default function CartItem({item, value}) {
+    const{id, title, img, price, total, count } = item;
+    const{increment, dercement, removeItem}= value;
     return (
-        <div>
-           hello is a CartItem
-          
+        <div className="row my-1 text-capitalize text-center" >
+            <div className="col-10 mx-auto col-lg-2" >
+                <img src={img} style={{width:"5rem", height:"5rem"}} className="img-fluid" alt="product"  />
+            </div>
+
+            <div className="col-10 mx-auto col-lg-2" >
+                <span className="d-lg-none" >product: </span> {title}
+            </div>
+
+            <div className="col-10 mx-auto col-lg-2">
+            <span className="d-lg-none" >price: </span> {price}
+            </div>
+
+            <div className="col-10 mx-auto col-lg-2 my-2 my-lg-0">
+                <div className="d-flex justify-content-center" >
+                    <div>
+                        <span className="btn btn-black mx-1" onClick={()=>dercement(id)} >-</span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
